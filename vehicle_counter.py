b36ee0596c613a13077e22b8e0a9a4fee9d96c5f
Onur Tosun <onurtosunn0@gmail.com>
2024-07-15 03:45:39 +0300

    yolo detector frame ile çalışıyor

--- vehicle_counter.py
+++ vehicle_counter.py
@@ -1,13 +1,17 @@
-import cv2
-import requests
-import time
-import signal
 import sys
+import cv2
+import time
+import torch
+import signal
+import requests
+from models.yolo import Model
+from yolo_detector import YOLODetection
 
 class VideoURLFinder:
-    def __init__(self, video_api_url, variable_url):
+
+    def __init__(self, video_api_url, configs_url):
         self.video_api_url = video_api_url
-        self.variable_url = variable_url
+        self.configs_url = configs_url
 
     def get_video_url(self):
         response = requests.get(self.video_api_url)
@@ -16,8 +20,8 @@ class VideoURLFinder:
         else:
             return None
     
-    def get_variable(self):
-        response = requests.get(self.variable_url)
+    def get_configs(self):
+        response = requests.get(self.configs_url)
         if response.status_code == 200:
             data = response.json()
             return data.get('config') 
@@ -25,17 +29,42 @@ class VideoURLFinder:
             return None
 
 class VideoProcessor:
-    def __init__(self, video_source_url_finder):
+
+    def __init__(self, video_source_url_finder, model_name):
+
         self.video_source_url_finder = video_source_url_finder
         self.current_video_url = None
         self.loop_count = 0
+        self.model_name = model_name
+        self.load_model()
+        self.yolo_detector = YOLODetection()
+    
+    def load_model(self):
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = torch.load(self.model_name, map_location=device) if isinstance(self.model_name, str) else self.model_name
+        if isinstance(self.model, dict):
+            self.model = self.model.get('ema' if self.model.get('ema') else 'model')
+        hub_model = Model(self.model.yaml).to(device)
+        hub_model.load_state_dict(self.model.float().state_dict())
+        self.loaded_model = hub_model.autoshape()
+        
+    def get_video_properties(self):
+
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def crop_lines(self, configs):
+        print(configs)
+
 
     def main(self):
+
         while True:
             new_video_url = self.video_source_url_finder.get_video_url()
-            variable = self.video_source_url_finder.get_variable()
-            print(new_video_url)
-            print(variable)
+            configs = self.video_source_url_finder.get_configs()
+
             if not new_video_url:
                 print("Failed to retrieve video URL!")
                 break
@@ -53,24 +82,24 @@ class VideoProcessor:
             ret, frame = self.cap.read()
             if not ret:
                 print("Video stream ended or failed to read.")
-                time.sleep(1)
                 continue
-
+            self.get_video_properties()
             self.loop_count += 1
-            print(f"Loop count: {self.loop_count}")
-            print(variable)
-            print(self.current_video_url)
+            end_point_1_value = configs['end_point_1']
+            print(configs)
+            print("*********", end_point_1_value, "**********")
 
-            cv2.imwrite("frame.jpg", frame)
+            self.yolo_detector.stream_video_with_detections_and_tracker(frame, self.loaded_model, self.model)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.cap.release()
                 break
-            time.sleep(1)
 
 if __name__ == "__main__":
     api_url = 'http://127.0.0.1:5001/get_video_url'
-    variable_url = 'http://127.0.0.1:5001/get_config'
-    video_source_url_finder = VideoURLFinder(api_url, variable_url)
-    processor = VideoProcessor(video_source_url_finder)
+    configs_url = 'http://127.0.0.1:5001/get_config'
+    model_name = "yolov7.pt"
+    
+    video_source_url_finder = VideoURLFinder(api_url, configs_url)
+    processor = VideoProcessor(video_source_url_finder, model_name)
     processor.main()
--- yolo_detector.py
+++ yolo_detector.py
@@ -1,54 +1,30 @@
-import os
 import cv2
-import torch
 import numpy as np
 from PIL import Image
-from models.yolo import Model
 from sort import Sort
-import subprocess as sp
 from collections import defaultdict
 
 class YOLODetection:
-    def __init__(self, model_name):
-        self.model_name = model_name
+    def __init__(self):
         self.stopped_threshold = 3
         self.background_color = (0, 0, 0)
         self.text_color = (255, 255, 255)
         self.stopped_objects = defaultdict(int)  
-        self.load_model()
         self.initialize_background_subtraction()
         self.initialize_sort_tracker()
 
-    def load_model(self):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = torch.load(self.model_name, map_location=device) if isinstance(self.model_name, str) else self.model_name
-        if isinstance(self.model, dict):
-            self.model = self.model.get('ema' if self.model.get('ema') else 'model')
-        hub_model = Model(self.model.yaml).to(device)
-        hub_model.load_state_dict(self.model.float().state_dict())
-        self.loaded_model = hub_model.autoshape()
-
     def initialize_background_subtraction(self):
+
         self.back_sub = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=100, detectShadows=True)
 
     def initialize_sort_tracker(self):
+
         self.sort_tracker = Sort(max_age=5, min_hits=2, iou_threshold=0.2)
 
-    def load_video(self, video_source):
-        self.cap = cv2.VideoCapture(video_source)
-        if not self.cap.isOpened():
-            raise ValueError("Could not open the video source")
-        self.get_video_properties()
-
-    def get_video_properties(self):
-        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    def perform_inference(self, frame):
+    def perform_inference(self, frame, loaded_model):
 
         converted_img = Image.fromarray(frame[:, :, ::-1], 'RGB')
-        detection_object = self.loaded_model(converted_img)
+        detection_object = loaded_model(converted_img)
         detection_result = detection_object.xyxy[0].cpu().detach().numpy()
         fg_mask = self.back_sub.apply(frame)
         return self.analyzer(fg_mask, detection_result)
@@ -76,6 +52,7 @@ class YOLODetection:
         return detections_to_sort
 
     def draw_detections(self, frame, detections):
+
         bbox_xyxy = detections[:, :4]
         obj_ids = detections[:, -1].astype(int)
         class_ids = detections[:, 4].astype(int)
@@ -100,56 +77,18 @@ class YOLODetection:
             cv2.rectangle(img, (x1, y1 - text_size[1] - 3), (x1 + text_size[0], y1 - 2), self.background_color, -1)
             cv2.rectangle(img, (x1, y1), (x2, y2), bbox_color, thickness=1, lineType=cv2.LINE_AA)
             cv2.putText(img, label, text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.3, self.text_color, 1)
+            cv2.imshow('frame', img)
+            cv2.waitKey(1)
 
     def track_objects_with_sort(self, detections):
         
         return self.sort_tracker.update(detections)
 
-    def stream_video_with_detections_and_sort(self, rtsp_url):
+    def stream_video_with_detections_and_tracker(self,frame, loaded_model, model):
 
-        self.start_ffplay(rtsp_url)
-        self.start_ffmpeg(rtsp_url)
-
-        while True:
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-
-            detections = self.perform_inference(frame)
-            tracked_detections = self.track_objects_with_sort(detections)
-            if len(tracked_detections) > 0:
-                self.draw_detections(frame, tracked_detections)
-
-            self.write_frame_to_ffmpeg(frame)
-
-        self.release_resources()
-
-    def start_ffplay(self, rtsp_url):
-        self.ffplay_process = sp.Popen(['ffplay', '-rtsp_flags', 'listen', rtsp_url])
-
-    def start_ffmpeg(self, rtsp_url):
-        command = [
-            'ffmpeg', '-re', '-y', '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24',
-            '-s', f"{self.width}x{self.height}", '-r', str(self.fps), '-i', '-', '-c:v', 'libx264',
-            '-preset', 'ultrafast', '-f', 'rtsp', '-rtsp_transport', 'tcp', '-muxdelay', '0.1',
-            '-bsf:v', 'dump_extra', rtsp_url
-        ]
-        self.ffmpeg_process = sp.Popen(command, stdin=sp.PIPE)
-
-    def write_frame_to_ffmpeg(self, frame):
-        self.ffmpeg_process.stdin.write(frame.tobytes())
-
-    def release_resources(self):
-        self.cap.release()
-        self.ffmpeg_process.stdin.close()
-        self.ffmpeg_process.wait()
-        self.ffplay_process.terminate()
-
-if __name__ == "__main__":
-    model_name = 'yolov7.pt'
-    video_source = 'Traffic.mp4'
-    output_rtsp_url = 'rtsp://localhost:8080/output'
-
-    yolo_detector = YOLODetection(model_name)
-    yolo_detector.load_video(video_source)
-    yolo_detector.stream_video_with_detections_and_sort(output_rtsp_url)
+        self.model = model
+        detections = self.perform_inference(frame, loaded_model)
+        tracked_detections = self.track_objects_with_sort(detections)
+        if len(tracked_detections) > 0:
+            self.draw_detections(frame, tracked_detections)
+            
